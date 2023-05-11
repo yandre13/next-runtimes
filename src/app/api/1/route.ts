b08429f1db4d3e99c29d055aa953a9638018c32f
yandre13 <yandre13.elsf@gmail.com>
2023-05-11 18:46:42 -0500

    edge v1

---
+++ src/app/api/1/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const runtime = 'edge'
+
+export async function GET(request: NextRequest) {
+  const res = await fetch('https://fakestoreapi.com/products/1')
+  const data = await res.json()
+
+  const { geo } = request
+  console.log(geo)
+  console.log(`Hello from ${geo?.city}, ${geo?.country}`)
+  const extra = {
+    city: geo?.city,
+    country: geo?.country,
+  }
+  return NextResponse.json({ data, extra })
+}
---
+++ src/components/button-1.tsx
@@ -0,0 +1,18 @@
+'use client'
+export default function Button1() {
+  const handleClick = async () => {
+    console.log('Click 1 happened')
+    const res = await fetch('/api/1')
+    const data = await res.json()
+    console.log(data)
+  }
+
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={handleClick}
+    >
+      Call to Action 1
+    </button>
+  )
+}
